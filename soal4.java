aaeb58b39ec6f7fa7c0cca4c4a89f5ac7d135cb4
Alfando <64246437+alfandovs@users.noreply.github.com>
2020-04-27 19:34:43 +0700

    Update soal4.java

--- soal4.java
+++ soal4.java
@@ -107,19 +107,4 @@ public class TextBook {
         return str;
     }
 }
-
-public class CourseDemo {
-
-    public static void main(String[] args) {
-       Instructor myInstructor = new Instructor("Ri", "Jung Hyeok", "AG0108");
-       
-       TextBook myTextBook = new TextBook("Aku Berdiri disini Untukmu", "Carrin Fuu", "Falcon");
-       
-       Course myCourse = new Course("Self Motivation", myInstructor, myTextBook);
-       
-       System.out.println(myCourse);
-    }
-    
-}
-
 }
